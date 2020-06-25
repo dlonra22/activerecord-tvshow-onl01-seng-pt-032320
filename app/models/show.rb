a0f1a1507d7dc0122431386f76626964bfc7f6f5
arnie b <arnold.bwaila@gmail.com>
2020-06-25 14:55:48 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -24,8 +24,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.shows_by_alphabetical_order
-      self.find(:all,
-  :order => 'users.username')
+      self.find(:all, :order => 'name')
   end
   
 end
