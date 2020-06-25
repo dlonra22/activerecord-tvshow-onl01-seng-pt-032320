48a11e9782cf738440e9e689cf84c0c203bb1780
arnie b <arnold.bwaila@gmail.com>
2020-06-25 14:29:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,18 @@
 class Show < ActiveRecord::Base 
   def self.highest_rating
-    Show.maximum(:rating)
+    self.maximum(:rating)
   end
   
   def self.most_popular_show
-    Show.find_by(rating: self.highest_rating)
+    self.find_by(rating: self.highest_rating)
+  end
+  
+  def self.lowest_rating
+      self.minimum(:rating)
   end
   
   def self.least_popular_show
-    Show.find_by(rating: self.minimum(:rating))
+    self.find_by(rating: self.lowest_rating)
   end
   
 end
